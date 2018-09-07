39a4324baa7b68d472fc324c0122756a20eac45d
rlassche <you@example.com>
2018-09-08 00:05:48 +0200

    .

---
+++ ssl/genAll2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#########################################################################################
+# Last update: 07-SEP-2018
+#
+# All steps.
+#########################################################################################
+./00_clean_server_certs.sh -c srv_rotterdam2.config \
+	&& ./01_ca_prep.sh \
+	&& ./02_ca_key.sh \
+	&& ./03_ca_cert.sh \
+	&& ./03_ca_cert.sh \
+	&& ./04_server_key.sh -c srv_rotterdam2.config \
+	&& ./05_server_sign_request.sh -c srv_rotterdam2.config \
+	&& ./06_ca_signs_server_sign_request.sh -c srv_rotterdam2.config \
+	&& ./07_ship_certificates.sh -c srv_rotterdam2.config
