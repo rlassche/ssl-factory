30460f472da3703d07660d6aebaedc7c9734ae83
rlassche <you@example.com>
2018-09-08 23:00:48 +0200

    .

--- ssl/genAll.sh
+++ ssl/genAll.sh
@@ -4,8 +4,8 @@
 #
 # All steps.
 #########################################################################################
-./00_clean_server_certs.sh -c srv_rotterdam.config \
-&& ./01_ca_prep.sh \
+#./00_clean_server_certs.sh -c srv_rotterdam.config \
+./01_ca_prep.sh \
 && ./02_ca_key.sh \
 && ./03_ca_cert.sh \
 && ./04_server_key.sh -c srv_rotterdam.config \
