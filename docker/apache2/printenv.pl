26886c9ae1317721d4146c798f59b5eb30806fb6
rlassche <you@example.com>
2018-09-09 17:18:47 +0200

    .

---
+++ docker/apache2/printenv.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+##
+##  printenv -- demo CGI program which just prints its environment
+##
+use utf8;
+#binmode STDOUT, ':encoding(UTF-8)';
+
+use Encode qw( decode encode ) ;
+#use Text::Unidecode;
+
+print "Access-Control-Allow-Origin: *\n";
+print "Content-type: text/html;\n\n";
+#print "Content-type: text/html; charset=iso-8859-1\n\n";
+#print "Content-type: text/html;charset=utf8\n\n";
+
+$str = "<h1>Car make Citroën (str)</h1>";
+#$str =  encode( 'utf8', $str ) ;
+print "<h1>Car make Citroën (hard coded)</h1>";
+print "$str";
+
+print "Perl default encoding: iso-8859-1<br>";
+print "<pre>";
+foreach $var (sort(keys(%ENV))) {
+    $val = $ENV{$var};
+    $val =~ s|\n|\\n|g;
+    $val =~ s|"|\\"|g;
+    print "${var}=\"${val}\"\n";
+}
+print "</pre>";
--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,20 @@
-checkCerts.sh -c ../ssl/srv_rotterdam.config
+./checkCerts.sh -c ../ssl/srv_rotterdam.config  
 
-#docker kill srv_rotterdam01
-#
-#docker rm srv_rotterdam01
-#
+if [ ! -d apache2/discount-2.2.4 ]
+then
+	cp -r /opt/discount-2.2.4 apache2/discount-2.2.4
+fi
+if [ ! -d apache2/apache-mod-markdown ]
+then
+	cp -r /opt/apache-mod-markdown apache2/apache-mod-markdown
+fi
+
+docker ps | grep srv_rotterdam01 | \
+	docker kill srv_rotterdam01
+
+docker ps -a | grep srv_rotterdam01 | \
+	docker rm srv_rotterdam01
+
 docker build --tag srv_rotterdam:v1 -f df_srv_rotterdam .
 
-#docker run -d -h srv_rotterdam01.local -p 9443:443 -p 9080:80 -v /tmp/share:/mnt --name srv_rotterdam01 srv_rotterdam:v1
+docker run -d -h srv_rotterdam01.local -p 9443:443 -p 9080:80 -v /tmp/share:/mnt --name srv_rotterdam01 srv_rotterdam:v1
